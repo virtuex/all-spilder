4f2847bf3ffa54605c8690ad5781981c211e5a41
xuda <1249505842@qq.com>
2018-10-01 20:17:39 +0800

    增加市区数据的处理

--- cwaler/src/main/java/Main.java
+++ cwaler/src/main/java/Main.java
@@ -1,9 +1,12 @@
 
+import org.apache.commons.lang3.time.DateUtils;
+import org.apache.poi.ss.usermodel.DateUtil;
 import virtue.dto.DbOpFactory;
 import virtue.pojo.Mortgage;
 import virtue.util.DataUtil;
 import virtue.util.PoiUtils;
 
+import javax.xml.crypto.Data;
 import java.util.List;
 import java.util.Map;
 
@@ -25,6 +28,7 @@ public class Main {
             Map<String, Object> newCert = DataUtil.isNewCert(securityNumber);
 
             String target = null;
+            //对常熟进行处理
             if (area.equals(DataUtil.CHANG_SHU)) {
                 if ((boolean) newCert.get("isNew")) {
 //                    System.out.println("新");
@@ -39,12 +43,53 @@ public class Main {
                         target = mortgage.getOwnerName() + "$" + newCert.get("securiNum");
                     }
                 }
-                mortgage.setSearch_target(target);
 
             }
+            //如果是高新区
+            if(area.equals(DataUtil.GAO_XIN_QU)){
+                //市区的年份、产权号规则和物权号码相同，可使用相同规则
+                String hisRightNum = mortgage.getHisRightNum();
+                Map<String, Object> newCert1 = DataUtil.isNewCert(hisRightNum);
+                //获取产权号
+                String securiNum = (String)newCert1.get("securiNum");
+                if(securiNum.length()==8){
+                    target = "02"+securiNum;
+                }
+                if(securiNum.length()==7){
+                    target = "020"+securiNum;
+                }
+            }
+
+            //如果是姑苏区、相城区、吴中区、工业园区、吴江区
+            if(area.equals(DataUtil.GU_SU_QU)||
+                    area.equals(DataUtil.XIANG_CHENG_QU)||
+                    area.equals(DataUtil.WU_ZHONG_QU)||
+                    area.equals(DataUtil.WU_JIANG_QU)||
+                    area.equals(DataUtil.YUAN_QU)){
+                //市区的年份、产权号规则和物权号码相同，可使用相同规则
+                String hisRightNum = mortgage.getHisRightNum();
+                Map<String, Object> newCert1 = DataUtil.isNewCert(hisRightNum);
+                Boolean isNew = (Boolean) newCert1.get("isNew");
+                if(isNew){
+                    //如果是新证，有日期和产权号
+                    String year = (String) newCert1.get("year");
+                    String securiNum = (String)newCert1.get("securiNum");
+                    target = year+securiNum;
+                }else{
+                    //如果是旧证，只有产权号
+                    String securiNum = (String)newCert1.get("securiNum");
+                    target = securiNum;
+                }
+            }
+            mortgage.setSearch_target(target);
+
+
             System.out.println(mortgage);
             instance.insert(mortgage);
+
         }
+        //释放资源
+        instance.close();
     }
 }
 
